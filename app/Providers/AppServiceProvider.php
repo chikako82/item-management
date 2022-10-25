0d6a67cb0006d20dd6abc846448d3a51ed9d1a89
chikako82 <chikakotakemura82@gmail.com>
2022-10-25 08:59:01 +0100

    httpsに修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (\App::enviroment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
